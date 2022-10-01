a13692ec57479442bb91476c7c145ca8a105aefe
KBluePurple <dbdnjstjr613@gmail.com>
2022-10-02 01:50:02 +0900

    init

---
+++ Assets/NodeManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+public class NodeManager : MonoBehaviour
+{
+    [SerializeField] private Transform[] nodes;
+    
+    public Vector2 GetNodePosition(Vector2Int position)
+    {
+        return nodes[position.x + position.y * 4].position;
+    }
+}
